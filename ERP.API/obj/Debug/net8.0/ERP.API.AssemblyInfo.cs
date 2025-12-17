45643052236676811b586839a0f88e933f6d0b42
gowthamr024 <gowtham.024.r@gmail.com>
2025-12-17 18:03:23 +0530

    add gitignore for build and IDE files

--- ERP.API/obj/Debug/net8.0/ERP.API.AssemblyInfo.cs
+++ ERP.API/obj/Debug/net8.0/ERP.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERP.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2a02a8b3cfe5955c1835aa26ef91fe16f949790")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERP.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
